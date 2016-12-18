27fcfbc9d9e875d7fa357f81c6856e858bcb695c
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2016-12-18 23:44:35 +1300

    Fixed eslint errors

--- src/app/config.js
+++ src/app/config.js
@@ -3,11 +3,11 @@ module.exports = {
     // Change this to your firebase configuration! (Add Firebase to your web app)
     FIREBASE_CONFIG: {
 
-        apiKey: "AIzaSyCLRHScHqDCIdwGNR2mSHQMUuNqS0qmHlo",
-        authDomain: "food-delivery-network.firebaseapp.com",
-        databaseURL: "https://food-delivery-network.firebaseio.com",
-        storageBucket: "food-delivery-network.appspot.com",
-        messagingSenderId: "1038809732852"
+        apiKey: 'AIzaSyCLRHScHqDCIdwGNR2mSHQMUuNqS0qmHlo',
+        authDomain: 'food-delivery-network.firebaseapp.com',
+        databaseURL: 'https://food-delivery-network.firebaseio.com',
+        storageBucket: 'food-delivery-network.appspot.com',
+        messagingSenderId: '1038809732852',
 
-    }
+    },
 };
