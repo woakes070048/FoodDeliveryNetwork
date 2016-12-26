99184eed9215405ec7ab5b894a4074a4a45d93e2
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2016-12-27 01:36:09 +1300

    Added register page

---
+++ src/app/components/user-register/user-register-container.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import UserRegisterPresentation from './user-register-presentation';
+
+const UserRegisterContainer = () =>
+    <UserRegisterPresentation />;
+
+export default UserRegisterContainer;
--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -6,10 +6,12 @@ import {
 import AppContainer from './components/app-container';
 import HomeContainer from './components/home/home-container';
 import UserLoginContainer from './components/user-login/user-login-container';
+import UserRegisterContainer from './components/user-register/user-register-container';
 
 export default (
     <Route path="/" component={AppContainer}>
         <IndexRoute component={HomeContainer} />
         <Route path="/login" component={UserLoginContainer} />
+        <Route path="/register" component={UserRegisterContainer} />
     </Route>
 );
