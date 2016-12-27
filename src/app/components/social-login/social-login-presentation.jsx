21b27e5365872051f09f7d9c2ef190ae90c41c5d
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2016-12-27 14:00:51 +1300

    Using now react-bootstrap grid to place social button at the right place

--- src/app/components/social-login/social-login-presentation.jsx
+++ src/app/components/social-login/social-login-presentation.jsx
@@ -2,7 +2,9 @@ import React, {
   PropTypes,
 } from 'react';
 import {
-    Col,
+  Col,
+  Grid,
+  Row,
 } from 'react-bootstrap';
 
 import 'bootstrap-social';
@@ -12,50 +14,85 @@ const SocialLoginPresentation = ({
     onTwitterButtonClicked,
     onGoogleButtonClicked,
     onGithubButtonClicked,
-}) =>
-    <Col
-        xs={12}
-        smOffset={1}
-        sm={10}
-        mdOffset={2}
-        md={8}
-        lgOffset={4}
-        lg={4}
-    >
-        <div className="social-buttons">
-            <a
-                className="btn btn-block btn-social btn-facebook"
-                onClick={() => onFacebookButtonClicked()}
-            >
-                <span className="fa fa-facebook" />
-                Sign in with Facebook
-            </a>
-
-            <a
-                className="btn btn-block btn-social btn-twitter"
-                onClick={() => onTwitterButtonClicked()}
-            >
-                <span className="fa fa-twitter" />
-                Sign in with Twitter
-            </a>
-
-            <a
-                className="btn btn-block btn-social btn-google"
-                onClick={() => onGoogleButtonClicked()}
-            >
-                <span className="fa fa-google" />
-                Sign in with Google
-            </a>
-
-            <a
-                className="btn btn-block btn-social btn-github"
-                onClick={() => onGithubButtonClicked()}
-            >
-                <span className="fa fa-github" />
-                Sign in with Github
-            </a>
-        </div>
-    </Col>;
+  }) =>
+  <Grid>
+      <Row>
+          <Col
+              xs={12}
+              sm={10}
+              smOffset={1}
+              md={8}
+              mdOffset={2}
+              lg={6}
+              lgOffset={3}
+          >
+              <a
+                 className="btn btn-block btn-social btn-facebook social-login-button"
+                 onClick={() => onFacebookButtonClicked()}
+              >
+                 <span className="fa fa-facebook" />
+                 Sign in with Facebook
+              </a>
+          </Col>
+      </Row>
+      <Row>
+          <Col
+              xs={12}
+              sm={10}
+              smOffset={1}
+              md={8}
+              mdOffset={2}
+              lg={6}
+              lgOffset={3}
+          >
+              <a
+                 className="btn btn-block btn-social btn-twitter social-login-button"
+                 onClick={() => onTwitterButtonClicked()}
+              >
+                 <span className="fa fa-twitter" />
+                 Sign in with Twitter
+              </a>
+          </Col>
+      </Row>
+      <Row>
+          <Col
+              xs={12}
+              sm={10}
+              smOffset={1}
+              md={8}
+              mdOffset={2}
+              lg={6}
+              lgOffset={3}
+          >
+      <a
+         className="btn btn-block btn-social btn-google social-login-button"
+         onClick={() => onGoogleButtonClicked()}
+      >
+         <span className="fa fa-google" />
+                 Sign in with Google
+              </a>
+          </Col>
+      </Row>
+      <Row>
+          <Col
+              xs={12}
+              sm={10}
+              smOffset={1}
+              md={8}
+              mdOffset={2}
+              lg={6}
+              lgOffset={3}
+          >
+              <a
+                 className="btn btn-block btn-social btn-github social-login-button"
+                 onClick={() => onGithubButtonClicked()}
+              >
+                 <span className="fa fa-github" />
+                 Sign in with Github
+              </a>
+          </Col>
+      </Row>
+  </Grid>;
 
 SocialLoginPresentation.propTypes = {
   onFacebookButtonClicked: PropTypes.func.isRequired,
