ea215379df991ffa0097150d56991a90eef50969
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2016-12-28 21:04:15 +1300

    Enhanced login and register page for different resolution

--- src/app/navigations/navbar-presentational.jsx
+++ src/app/navigations/navbar-presentational.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {
     Navbar,
-    Grid, Row, Col,
 } from 'react-bootstrap';
 import {
   IndexLinkContainer,
@@ -9,30 +8,18 @@ import {
 import NavbarUserMenuPresentational from './navbar-usermenu-presentational';
 
 const NavbarPresentational = () =>
-  <Grid>
-    <Row>
-      <Col
-        xs={11}
-        sm={11}
-        md={11}
-        lg={11}
-      >
-        <Navbar>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <IndexLinkContainer to="/">
-                <a> Food Delivery Network </a>
-              </IndexLinkContainer>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Collapse>
-            <NavbarUserMenuPresentational />
-          </Navbar.Collapse>
-        </Navbar>
-
-      </Col>
-    </Row>
-  </Grid>;
+  <Navbar>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <IndexLinkContainer to="/">
+          <a> Food Delivery Network </a>
+        </IndexLinkContainer>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <NavbarUserMenuPresentational />
+    </Navbar.Collapse>
+  </Navbar>;
 
 export default NavbarPresentational;
