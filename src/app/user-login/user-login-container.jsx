365c6cdb010457de2c7b2aa5d0d4aee94e16bcd9
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2016-12-30 18:49:23 +1300

    Redirecting to index page in both login and register if the user already signed in

--- src/app/user-login/user-login-container.jsx
+++ src/app/user-login/user-login-container.jsx
@@ -1,4 +1,40 @@
-import React from 'react';
+import React, {
+  Component,
+} from 'react';
+import {
+  connect,
+} from 'react-redux';
+import {
+  browserHistory,
+} from 'react-router';
 import UserLoginPresentational from './user-login-presentational';
 
-export default () => <UserLoginPresentational />;
+class UserLoginContainer extends Component {
+  componentWillUpdate(nextProps) {
+    if (nextProps.userExists) {
+      browserHistory.push('/');
+    }
+  }
+
+  render() {
+    return (
+      <UserLoginPresentational />
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  const userInfo = state.firebase.userInfo;
+
+  if (userInfo.userExists) {
+    return {
+      userExists: true,
+    };
+  }
+
+  return {
+    userExists: false,
+  };
+}
+
+export default connect(mapStateToProps)(UserLoginContainer);
--- src/app/user-register/user-register-container.jsx
+++ src/app/user-register/user-register-container.jsx
@@ -1,4 +1,40 @@
-import React from 'react';
+import React, {
+  Component,
+} from 'react';
+import {
+  connect,
+} from 'react-redux';
+import {
+  browserHistory,
+} from 'react-router';
 import UserRegisterPresentational from './user-register-presentational';
 
-export default () => <UserRegisterPresentational />;
+class UserRegisterContainer extends Component {
+  componentWillUpdate(nextProps) {
+    if (nextProps.userExists) {
+      browserHistory.push('/');
+    }
+  }
+
+  render() {
+    return (
+      <UserRegisterPresentational />
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  const userInfo = state.firebase.userInfo;
+
+  if (userInfo.userExists) {
+    return {
+      userExists: true,
+    };
+  }
+
+  return {
+    userExists: false,
+  };
+}
+
+export default connect(mapStateToProps)(UserRegisterContainer);
