9f6cb62be50423b0054e8137dfac516633458644
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-01-01 23:09:33 +1300

    Added error validation to user sign up

--- src/app/user-signup/username-email-password-container.jsx
+++ src/app/user-signup/username-email-password-container.jsx
@@ -11,12 +11,39 @@ import * as firebaseActions from '../firebase/actions';
 import UsernameEmailPasswordPresentational from './username-email-password-presentational';
 
 class UsernameEmailPasswordContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.validateState = this.validateState.bind(this);
+    this.onSignUpClicked = this.onSignUpClicked.bind(this);
+  }
+
+  onSignUpClicked(usernameOrEmailAddress, password) {}
+
+  validateState(usernameOrEmailAddress, password, reEnteredPassword) {
+    return {
+      usernameOrEmailAddressValidationResult: usernameOrEmailAddress ? null : 'error',
+      usernameOrEmailAddressValidationMessage: usernameOrEmailAddress ? null : 'Username or email is required.',
+      passwordValidationResult: password ? null : 'error',
+      passwordValidationMessage: password ? null : 'Password is required.',
+      reEnteredPasswordValidationResult: reEnteredPassword === password ? null : 'error',
+      reEnteredPasswordValidationMessage: reEnteredPassword === password ? null : 'Should match the above Password.',
+    };
+  }
+
   render() {
-    return (< UsernameEmailPasswordPresentational />);
+    return (
+      <UsernameEmailPasswordPresentational
+        onSignUpClicked={(usernameOrEmailAddress, password) =>
+              this.onSignUpClicked(usernameOrEmailAddress, password)}
+        validateState={(usernameOrEmailAddress, password, reEnteredPassword) =>
+              this.validateState(usernameOrEmailAddress, password, reEnteredPassword)}
+      />
+    );
   }
 }
 
-function mapStateToProps(state, ownProps) {
+function mapStateToProps(state) {
   return state;
 }
 
--- src/app/user-signup/username-email-password-presentational.jsx
+++ src/app/user-signup/username-email-password-presentational.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, {
+  Component,
+  PropTypes,
+} from 'react';
 import {
   Button,
+  HelpBlock,
   Form,
   FormControl,
   FormGroup,
@@ -8,41 +12,181 @@ import {
   InputGroup,
 } from 'react-bootstrap';
 
-const UsernameEmailPasswordPresentational = () =>
-  <Form horizontal>
-    <FormGroup>
-      <InputGroup>
-        <InputGroup.Addon>
-          <Glyphicon glyph="user" />
-        </InputGroup.Addon>
-        <FormControl type="text" placeholder="Username or email" />
-      </InputGroup>
-    </FormGroup>
-    <FormGroup>
-      <InputGroup>
-        <InputGroup.Addon>
-          <Glyphicon glyph="lock" />
-        </InputGroup.Addon>
-        <FormControl type="password" placeholder="Password" />
-      </InputGroup>
-    </FormGroup>
-    <FormGroup>
-      <InputGroup>
-        <InputGroup.Addon>
-          <Glyphicon glyph="lock" />
-        </InputGroup.Addon>
-        <FormControl type="password" placeholder="Re-enter password" />
-      </InputGroup>
-    </FormGroup>
-    <FormGroup>
-      <Button
-        type="submit"
-        bsStyle="primary"
-        block
-      >
+class UsernameEmailPasswordPresentational extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      usernameOrEmailAddressValidationResult: null,
+      usernameOrEmailAddressValidationMessage: '',
+      usernameOrEmailAddress: this.props.initialUsernameOrEmailAddress,
+      usernameOrEmailAddressChanged: false,
+      passwordValidationResult: null,
+      passwordValidationMessage: '',
+      password: '',
+      passwordChanged: false,
+      reEnteredPasswordValidationResult: null,
+      reEnteredPasswordValidationMessage: '',
+      reEnteredPassword: '',
+      reEnteredPasswordChanged: false,
+    };
+
+    this.onUsernameOrEmailAddressChanged = this.onUsernameOrEmailAddressChanged.bind(this);
+    this.onPasswordChanged = this.onPasswordChanged.bind(this);
+    this.onReEnteredPasswordChanged = this.onReEnteredPasswordChanged.bind(this);
+    this.validateState = this.validateState.bind(this);
+    this.validateUsernameOrEmailAddress = this.validateUsernameOrEmailAddress.bind(this);
+    this.validatePassword = this.validatePassword.bind(this);
+    this.validateReEnteredPassword = this.validateReEnteredPassword.bind(this);
+    this.onSignUpClicked = this.onSignUpClicked.bind(this);
+  }
+
+  onUsernameOrEmailAddressChanged(e) {
+    this.setState(Object.assign(this.state, {
+      usernameOrEmailAddress: e.target.value,
+      usernameOrEmailAddressChanged: true,
+    }));
+
+    this.validateState();
+  }
+
+  onPasswordChanged(e) {
+    this.setState(Object.assign(this.state, {
+      password: e.target.value,
+      passwordChanged: true,
+    }));
+
+    this.validateState();
+  }
+
+  onReEnteredPasswordChanged(e) {
+    this.setState(Object.assign(this.state, {
+      reEnteredPassword: e.target.value,
+      reEnteredPasswordChanged: true,
+    }));
+
+    this.validateState();
+  }
+
+  onSignUpClicked() {
+    this.props.onSignUpClicked(this.state.usernameOrEmailAddress, this.state.password);
+  }
+
+  validateUsernameOrEmailAddress() {
+    return this.state.usernameOrEmailAddressValidationResult;
+  }
+
+  validatePassword() {
+    return this.state.passwordValidationResult;
+  }
+
+  validateReEnteredPassword() {
+    return this.state.reEnteredPasswordValidationResult;
+  }
+
+  validateState() {
+    const {
+      usernameOrEmailAddressValidationResult,
+      usernameOrEmailAddressValidationMessage,
+      passwordValidationResult,
+      passwordValidationMessage,
+      reEnteredPasswordValidationResult,
+      reEnteredPasswordValidationMessage,
+    } = this.props.validateState(this.state.usernameOrEmailAddress, this.state.password, this.state.reEnteredPassword);
+
+    this.setState(Object.assign(this.state, {
+      usernameOrEmailAddressValidationResult: this.state.usernameOrEmailAddressChanged ?
+        usernameOrEmailAddressValidationResult : null,
+      usernameOrEmailAddressValidationMessage,
+      passwordValidationResult: this.state.passwordChanged ? passwordValidationResult : null,
+      passwordValidationMessage,
+      reEnteredPasswordValidationResult: this.state.reEnteredPasswordChanged ?
+        reEnteredPasswordValidationResult : null,
+      reEnteredPasswordValidationMessage,
+    }));
+  }
+
+  render() {
+    const usernameOrEmailAddressHelpBlock = this.state.usernameOrEmailAddressValidationResult ?
+      <HelpBlock>{this.state.usernameOrEmailAddressValidationMessage}</HelpBlock> :
+      <div />;
+    const passwordHelpBlock = this.state.passwordValidationResult ?
+      <HelpBlock>{this.state.passwordValidationMessage}</HelpBlock> :
+      <div />;
+    const reEnteredPasswordHelpBlock = this.state.reEnteredPasswordValidationResult ?
+      <HelpBlock>{this.state.reEnteredPasswordValidationMessage}</HelpBlock> :
+      <div />;
+
+    return (
+      <Form horizontal>
+        <FormGroup validationState={this.validateUsernameOrEmailAddress()}>
+          <InputGroup>
+            <InputGroup.Addon>
+              <Glyphicon glyph="user" />
+            </InputGroup.Addon>
+            <FormControl
+              type="text"
+              placeholder="Username or email"
+              value={this.state.usernameOrEmailAddress}
+              onChange={this.onUsernameOrEmailAddressChanged}
+            />
+          </InputGroup>
+          <FormControl.Feedback />
+          {usernameOrEmailAddressHelpBlock}
+        </FormGroup>
+        <FormGroup validationState={this.validatePassword()}>
+          <InputGroup>
+            <InputGroup.Addon>
+              <Glyphicon glyph="lock" />
+            </InputGroup.Addon>
+            <FormControl
+              type="password"
+              placeholder="Password"
+              value={this.state.password}
+              onChange={this.onPasswordChanged}
+            />
+          </InputGroup>
+          <FormControl.Feedback />
+          {passwordHelpBlock}
+        </FormGroup>
+        <FormGroup validationState={this.validateReEnteredPassword()}>
+          <InputGroup>
+            <InputGroup.Addon>
+              <Glyphicon glyph="lock" />
+            </InputGroup.Addon>
+            <FormControl
+              type="password"
+              placeholder="Re-enter password"
+              value={this.state.reEnteredPassword}
+              onChange={this.onReEnteredPasswordChanged}
+            />
+          </InputGroup>
+          <FormControl.Feedback />
+          {reEnteredPasswordHelpBlock}
+        </FormGroup>
+        <FormGroup>
+          <Button
+            type="submit"
+            bsStyle="primary"
+            block
+            onClick={this.onSignUpClicked}
+          >
        Sign up
      </Button>
-    </FormGroup>
-  </Form>;
+        </FormGroup>
+      </Form>
+    );
+  }
+}
+
+UsernameEmailPasswordPresentational.propTypes = {
+  initialUsernameOrEmailAddress: PropTypes.string,
+  onSignUpClicked: PropTypes.func.isRequired,
+  validateState: PropTypes.func.isRequired,
+};
+
+UsernameEmailPasswordPresentational.defaultProps = {
+  initialUsernameOrEmailAddress: '',
+};
 
 export default UsernameEmailPasswordPresentational;
