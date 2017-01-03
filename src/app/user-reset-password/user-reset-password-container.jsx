a3594f5107609e414faf47c77ea1817d82c38606
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-01-03 17:47:43 +1300

    Added validation to reset password page

--- src/app/user-reset-password/user-reset-password-container.jsx
+++ src/app/user-reset-password/user-reset-password-container.jsx
@@ -1,23 +1,111 @@
+import emailValidator from 'email-validator';
 import React, {
   Component,
+  PropTypes,
 } from 'react';
+import {
+  bindActionCreators,
+} from 'redux';
 import {
   connect,
 } from 'react-redux';
+import * as firebaseActions from '../firebase/actions';
+import * as notificationActions from '../notification/actions';
 import UserResetPasswordPresentational from './user-reset-password-presentational';
 
 class UserResetPasswordContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.validateState = this.validateState.bind(this);
+    this.onResetPasswordClicked = this.onResetPasswordClicked.bind(this);
+    this.checkIfEmailAddressProvided = this.checkIfEmailAddressProvided.bind(this);
+    this.checkIfEmailAddressFromattedCorrectly = this.checkIfEmailAddressFromattedCorrectly.bind(this);
+
+    this.state = {
+      lastOperationId: '',
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.state.lastOperationId) {
+      const lastOperation =
+        nextProps.operations.find(operation => operation.operationId === this.state.lastOperationId);
+
+      if (lastOperation) {
+        if (lastOperation.failed) {
+          this.props.notificationActions.addError(lastOperation.errorMessage);
+        }
+
+        this.props.firebaseActions.acknowledgeOperaation(lastOperation.operationId);
+      }
+    }
+  }
+
+  onResetPasswordClicked(emailAddress) {
+  }
+
+  checkIfEmailAddressProvided(emailAddress, validationMessages) {
+    if (validationMessages.emailAddressValidationMessage) {
+      return validationMessages;
+    }
+
+    return emailAddress ? validationMessages : Object.assign(validationMessages, {
+      emailAddressValidationMessage: 'Email address is required.',
+    });
+  }
+
+  checkIfEmailAddressFromattedCorrectly(emailAddress, validationMessages) {
+    if (validationMessages.emailAddressValidationMessage) {
+      return validationMessages;
+    }
+
+    return emailValidator.validate(emailAddress) ? validationMessages : Object.assign(validationMessages, {
+      emailAddressValidationMessage: 'Email address is badly formatted.',
+    });
+  }
+
+  validateState(emailAddress) {
+    let validationMessages = {
+      emailAddressValidationMessage: null,
+    };
+
+    validationMessages = this.checkIfEmailAddressProvided(emailAddress, validationMessages);
+    validationMessages = this.checkIfEmailAddressFromattedCorrectly(emailAddress, validationMessages);
+
+    return Object.assign(validationMessages, {
+      emailAddressValidationResult: validationMessages.emailAddressValidationMessage ? 'error' : null,
+    });
+  }
+
   render() {
     return (
-      <UserResetPasswordPresentational />
+      <UserResetPasswordPresentational
+        onResetPasswordClicked={emailAddress =>
+              this.onResetPasswordClicked(emailAddress)}
+        validateState={emailAddress =>
+              this.validateState(emailAddress)}
+      />
     );
   }
 }
 
+UserResetPasswordContainer.propTypes = {
+  firebaseActions: PropTypes.object.isRequired,
+  notificationActions: PropTypes.object.isRequired,
+};
+
 function mapStateToProps(state) {
   return {
-    userExists: state.firebase.userInfo.userExists,
+    operations: state.firebase.operations,
   };
 }
 
-export default connect(mapStateToProps)(UserResetPasswordContainer);
+function mapDispatchToProps(dispatch) {
+  return {
+    firebaseActions: bindActionCreators(firebaseActions, dispatch),
+    notificationActions: bindActionCreators(notificationActions, dispatch),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserResetPasswordContainer);
--- src/app/user-reset-password/user-reset-password-presentational.jsx
+++ src/app/user-reset-password/user-reset-password-presentational.jsx
@@ -1,6 +1,122 @@
-import React from 'react';
+import React, {
+  Component,
+  PropTypes,
+} from 'react';
+import {
+  Button,
+  Col,
+  HelpBlock,
+  Form,
+  FormControl,
+  FormGroup,
+  Glyphicon,
+  InputGroup,
+} from 'react-bootstrap';
 
-const UserResetPasswordPresentational = () =>
-  <div>Reset Password</div>;
+class UserResetPasswordPresentational extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      emailAddressValidationResult: null,
+      emailAddressValidationMessage: '',
+      emailAddress: this.props.initialEmailAddress,
+      emailAddressChanged: false,
+      resetPasswordClicked: false,
+    };
+
+    this.onEmailAddressChanged = this.onEmailAddressChanged.bind(this);
+    this.validateState = this.validateState.bind(this);
+    this.onResetPasswordClicked = this.onResetPasswordClicked.bind(this);
+  }
+
+  onEmailAddressChanged(e) {
+    this.setState(Object.assign(this.state, {
+      emailAddress: e.target.value,
+      emailAddressChanged: true,
+    }));
+
+    this.validateState();
+  }
+
+  onResetPasswordClicked() {
+    this.setState(Object.assign(this.state, {
+      resetPasswordClicked: true,
+    }));
+
+    this.validateState();
+
+    if (!this.state.emailAddressValidationResult && !this.state.passwordValidationResult) {
+      this.props.onResetPasswordClicked(this.state.emailAddress, this.state.password);
+    }
+  }
+
+  validateState() {
+    const {
+      emailAddressValidationResult,
+      emailAddressValidationMessage,
+    } = this.props.validateState(this.state.emailAddress);
+
+    this.setState(Object.assign(this.state, {
+      emailAddressValidationResult: this.state.resetPasswordClicked || this.state.emailAddressChanged ?
+        emailAddressValidationResult : null,
+      emailAddressValidationMessage,
+    }));
+  }
+
+  render() {
+    const emailAddressHelpBlock = this.state.emailAddressValidationResult ?
+      <HelpBlock>{this.state.emailAddressValidationMessage}</HelpBlock> :
+      <div />;
+
+    return (
+      <Col
+        smOffset={2}
+        sm={8}
+        mdOffset={3}
+        md={6}
+        lgOffset={4}
+        lg={4}
+      >
+        <Form horizontal>
+          <FormGroup validationState={this.state.emailAddressValidationResult}>
+            <InputGroup>
+              <InputGroup.Addon>
+                <Glyphicon glyph="user" />
+              </InputGroup.Addon>
+              <FormControl
+                type="email"
+                placeholder="Email address"
+                value={this.state.emailAddress}
+                onChange={this.onEmailAddressChanged}
+              />
+            </InputGroup>
+            <FormControl.Feedback />
+            {emailAddressHelpBlock}
+          </FormGroup>
+          <FormGroup>
+            <Button
+              bsStyle="primary"
+              block
+              onClick={this.onResetPasswordClicked}
+            >
+            Reset password
+          </Button>
+          </FormGroup>
+        </Form>
+      </Col>
+    );
+  }
+}
+
+UserResetPasswordPresentational.propTypes = {
+  initialEmailAddress: PropTypes.string,
+  onResetPasswordClicked: PropTypes.func.isRequired,
+  validateState: PropTypes.func.isRequired,
+};
+
+UserResetPasswordPresentational.defaultProps = {
+  initialEmailAddress: '',
+};
 
 export default UserResetPasswordPresentational;
