91e18e89fca751a461c9b44d1f8c70c30f99a341
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-01-07 18:24:28 +1300

    Added send email verification action to firebase namespace

---
+++ src/app/user-profile/emails/emails-presentational.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  Col,
+} from 'react-bootstrap';
+
+const EmailsPresentational = () =>
+  <Col
+        sm={6}
+        md={6}
+        lg={4}
+      >
+        <h3>Emails</h3>
+        <div className="form-divider" />
+        emails
+  </Col>;
+
+export default EmailsPresentational;
