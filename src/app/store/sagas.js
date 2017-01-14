b5da9290b2f41430a9f4b0ae1b604611ae7ebbef
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-01-15 10:33:37 +1300

    Merged all sagas into one single file

--- src/app/store/sagas.js
+++ src/app/store/sagas.js
@@ -1,15 +1,17 @@
 /* jshint esversion: 6 */
 
-import watchFetchUser from '../user-access/sagas/fetch-user';
-import watchResetPassword from '../user-access/sagas/reset-password';
-import watchSendEmailVerification from '../user-access/sagas/send-email-verification';
-import watchSignUpWithEmailAndPassword from '../user-access/sagas/signup-with-email-password';
-import watchSignUpWithProvider from '../user-access/sagas/signup-with-provider';
-import watchSignInWithEmailAndPassword from '../user-access/sagas/signin-with-email-password';
-import watchSignInWithProvider from '../user-access/sagas/signin-with-provider';
-import watchSignOut from '../user-access/sagas/signout';
-import watchUpdateUserPublicProfile from '../user-access/sagas/update-user-public-profile';
-import watchUpdatePassword from '../user-access/sagas/update-password';
+import {
+  watchFetchUser,
+  watchResetPassword,
+  watchSendEmailVerification,
+  watchSignUpWithEmailAndPassword,
+  watchSignUpWithProvider,
+  watchSignInWithEmailAndPassword,
+  watchSignInWithProvider,
+  watchSignOut,
+  watchUpdateUserPublicProfile,
+  watchUpdatePassword,
+} from '../user-access/sagas';
 
 export default function* sagas() {
   yield [watchFetchUser(),
